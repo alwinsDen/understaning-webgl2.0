aaaf1788457ebe3115967d07bf317f244323e589
alwinvarghese <jsmadesimple@gmail.com>
2024-05-12 21:07:23 +0530

    clip space to screen space direct via glsl.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function createShader(gl, type, source) {
     if (success) {
         return shader;
     }
-    console.log(gl.getShaderInfoLog(shader));  // eslint-disable-line
+    console.log(gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
     return undefined;
 }
@@ -46,12 +46,16 @@ async function main() {
     var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
     var program = createProgram(gl, vertexShader, fragmentShader);
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
     var positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     var positions = [
-        0, 0,
-        0, 0.5,
-        0.7, 0,
+        10, 20,
+        80, 20,
+        10, 30,
+        10, 30,
+        80, 20,
+        80, 30,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
     var vao = gl.createVertexArray();
@@ -69,9 +73,10 @@ async function main() {
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program);
     gl.bindVertexArray(vao);
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
     var primitiveType = gl.TRIANGLES;
     var offset = 0;
-    var count = 3;
+    var count = 6;
     gl.drawArrays(primitiveType, offset, count);
 }
 
